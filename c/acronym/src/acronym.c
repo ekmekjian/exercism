6619d4d98b576957ac30d80a43d6e28f77cb7d9f
Spawndemonic <spawndemonic@gmail.com>
2019-02-03 21:43:47 -0500

    updated acronym.c

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -1,3 +1,15 @@
-include <stdlib>
+#include <stdlib>
+#include <string>
 
+char * acronymGen(char * str)
+{
+    int len = strlen(str)
+    char * i;
+    char strin[len];
+    for(i = str;*i;i++)
+    {
+        strcat(strin,i);
+    }
+    return acronym;
+}
 
