821cd5974ce3476e18e56a156db5c278bf238426
Spawndemonic <spawndemonic@gmail.com>
2019-02-10 14:12:43 -0500

    edits

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <stdio.h>
 
-bool IsIsogram(char *str)
+bool is_isogram(const char str[])
 {
     bool isIso = true;
 //loop through each letter to find dupilcate
-  	char * i; 
+  	const char * i; 
     for(i=str;*i;i++)
     {
-		char *j;
+		const char *j;
 	  for(j = str;*j;j++)
          {
             if(i==j)
@@ -24,3 +24,4 @@ bool IsIsogram(char *str)
       //return false if nothing found
       else {return false;}
  }
+
