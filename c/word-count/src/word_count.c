3e150fb5dfa3c57dcc06b1849c5695d53cd23dac
spawndemonic <spawndemonic@gmail.com>
2019-04-21 11:16:03 -0400

    Update word_count.c

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -7,19 +7,38 @@ int word_count(const char *input_text, word_count_word_t * words)
 {
     int totalCount = 0;
     char wordList[MAX_WORDS];
-    //count each iteration of the words
-   
-    //initialze array to pointer
     char * token;
     //Seperate each word in input_text to an array and words[index].text
     token = strtok(input_text," ");
     while(token !=NULL)
     {
+        if(strlen(token)>MAX_WORD_LENGTH)
+        {
+            return -1;
+        }
          //loop through each word in words comparing each to words in the array
+        //count each iteration of the words
         wordList[totalCount]=token;
-      
         token = strtok(NULL," ");
         totalCount++;
     }
+    if(totalCount>MAX_WORDS)
+    {
+        return -2;
+    }
+      for(int i=0;i<totalCount;i++)
+      {
+          if(words[0].text==NULL)
+          {
+              words[0].text == wordList[i];
+          }
+          else
+          {
+                      
+          }
+      }
     return totalCount;
 }
+
+
+
